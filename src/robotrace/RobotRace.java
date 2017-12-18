80f3b612e99e9469c6719f63f5ca3557a16a3d0e
Claudiu Ion <ioncld97@gmail.com>
2017-12-18 16:58:51 +0100

    Added new comment line (from NetBeans)

--- src/robotrace/RobotRace.java
+++ src/robotrace/RobotRace.java
@@ -302,3 +302,4 @@ public class RobotRace extends Base {
 
 // Adding new comments to repository for testing
 // Comment added inside Github WebIDE (testing pulling inside NetBeans)
+// Adding comment #1 (from inside NetBeans IDE)
