2ffcb374efd22fda2ca4b2168e266b04604dc357
Claudiu Ion <ioncld97@gmail.com>
2017-12-18 16:48:23 +0100

    Robot Race initial commit (original files)

---
+++ src/robotrace/shaderPrograms/Default/vertex.glsl
@@ -0,0 +1,8 @@
+#version 120
+// simple vertex shader
+
+void main()
+{
+    gl_Position    = gl_ModelViewProjectionMatrix * gl_Vertex;      // model view transform
+    gl_FrontColor = gl_Color;
+}
